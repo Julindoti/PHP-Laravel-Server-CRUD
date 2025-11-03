831d812c1fb29e612f2fe9b3c503af2551f10778
Dev-Adrianoo <adrianosouzaa733@gmail.com>
2025-11-03 16:40:56 -0300

    feat: add route to signIn

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,11 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
+
+Route::get('/', function () {
+    return Inertia::render('SignInPage');
+});
+
 Route::get('/signin', function () {
     return Inertia::render('SignInPage');
 });
